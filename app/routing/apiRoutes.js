ae7483ece517608e30a378fe71d5efa82bc04b66
Meghedi Hacopi <hacopian.m@gmail.com>
2020-03-23 01:44:10 -0700

    aaded modal popup

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -13,6 +13,7 @@ module.exports = function (app) {
             const element = friendsData[i];
             for (let j = 0; j < element.answers.length; j++) {
                 const element2 = parseInt(element.answers[j]);
+                debugger;
                 if (req.body.answers[j] !== element2) {
                     let differnce = Math.abs(req.body.answers[j] - element2);
                     totalDifference +=differnce;
